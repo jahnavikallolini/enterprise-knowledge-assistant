ee5b8dcc157ef3479132cf4a45fcaea90e5a4bec
jahnavikallolini <jahnavikallolini@gmail.com>
2026-07-08 11:27:38 +0530

    Configure CORS for deployed frontend

--- backend/main.py
+++ backend/main.py
@@ -18,8 +18,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-
-        "https://enterprise-knowledge-assistant-sigma.vercel.app/",
+        "https://enterprise-knowledge-assistant-h7v06ttuf-jkall.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
